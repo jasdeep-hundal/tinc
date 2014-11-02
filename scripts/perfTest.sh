b30983371e10f478f8e38fd6bd3affd965a198c7
jasdeep-hundal <jasdeep.singh.hundal@gmail.com>
2014-11-02 14:57:03 -0500

    Add perf and strace monitoring

--- scripts/perfTest.sh
+++ scripts/perfTest.sh
@@ -19,23 +19,35 @@ EOD
 )
 }
 
+# Names for output files for this run
+TIMESTAMP=`date +%s`
+TINCD_PERF_FILE="tinc_perf-$TIMESTAMP.dat"
+TINCD_STRACE_FILE="tinc_strace-$TIMESTAMP.txt"
+NETPERF_PERF_FILE="netperf_perf-$TIMESTAMP.dat"
+NETPERF_TINC_PERF_FILE="netperf_through_tinc_perf-$TIMESTAMP.dat"
+
 # Start tincd on this host and get its process id
 sudo tincd -n $NETWORK_NAME
 LOCAL_TINCD_PID=`pgrep tincd`
-# Start tincd on the remote host and get its process id
+# Start tincd on the remote host
 remote_host_command "sudo tincd -n $NETWORK_NAME" > /dev/null
-# Start netperf server on remote host and get its process id
+# Start netperf server on remote host
 remote_host_command "nohup netserver" > /dev/null
 
 echo "Testing bulk data performance w/o tinc VPN"
-netperf -H $REMOTE_HOST_IP
+sudo perf record  -g -o $NETPERF_PERF_FILE -- netperf -H $REMOTE_HOST_IP
+
+sudo perf record  -g -o $TINCD_PERF_FILE -p $LOCAL_TINCD_PID &
+sudo strace -c -p $LOCAL_TINCD_PID -o $TINCD_STRACE_FILE &
 
 echo "Testing bulk data performance w/ tinc VPN"
-netperf -H $REMOTE_HOST_TINC_IP
+sudo perf record -g -o $NETPERF_TINC_PERF_FILE -- netperf -H $REMOTE_HOST_TINC_IP
 
 # Shut down netserver on remote host
 remote_host_command "sudo pkill -3 netserver" > /dev/null
 # Shut down tincd on remote host
 remote_host_command "sudo pkill -3 tincd" > /dev/null
-# Shut down tincd on this host
+# Shut down processes on this host
+sudo pkill -3 strace
+sudo pkill -3 perf
 sudo pkill -3 tincd
