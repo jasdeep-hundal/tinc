cb23f57248d5be2b5e9eb42409d9f7f433e663aa
Derek Chiang <derekchiang93@gmail.com>
2014-11-24 01:37:54 -0500

    less debug msg

--- src/net_packet.c
+++ src/net_packet.c
@@ -954,7 +954,6 @@ bool receive_sptps_record(void *handle, uint8_t type, const char *data, uint16_t
   send a packet to the given vpn ip.
 */
 void send_packet(node_t *n, vpn_packet_t *packet) {
-    logger(DEBUG_ALWAYS, LOG_DEBUG, "send_packet!");
 	node_t *via;
 
 	if(n == myself) {
@@ -979,7 +978,6 @@ void send_packet(node_t *n, vpn_packet_t *packet) {
 	n->out_bytes += packet->len;
 
 	if(n->status.sptps) {
-        logger(DEBUG_ALWAYS, LOG_DEBUG, "send_sptps_packet!");
 		send_sptps_packet(n, packet);
 		return;
 	}
@@ -991,11 +989,9 @@ void send_packet(node_t *n, vpn_packet_t *packet) {
 			   n->name, via->name, n->via->hostname);
 
 	if(packet->priority == -1 || ((myself->options | via->options) & OPTION_TCPONLY)) {
-        logger(DEBUG_ALWAYS, LOG_DEBUG, "send_tcppacket!");
 		if(!send_tcppacket(via->connection, packet))
 			terminate_connection(via->connection, true);
     } else {
-        logger(DEBUG_ALWAYS, LOG_DEBUG, "send_udppacket!");
 		send_udppacket(via, packet);
     }
 }
